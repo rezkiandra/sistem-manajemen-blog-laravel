feb1214e94be4f612fe562d4dcc2cc6774d4ccda
LEVI <rezkiandra4123@gmail.com>
2024-09-25 00:32:10 +0700

    rev: revitions routes resource

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdsenseController;
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DomainController;
 use App\Http\Controllers\ProviderController;
@@ -17,53 +18,27 @@ Route::middleware(['web'])->group(function () {
 });
 
 Route::middleware(['web'])->group(function () {
-	Route::controller(AdsenseController::class)->prefix('admin/adsense')->group(function () {
-		Route::get('/', 'index')->name('adsense.index');
-		Route::get('/create', 'create')->name('adsense.create');
-		Route::post('/store', 'store')->name('adsense.store');
-		Route::get('/show/{adsense}', 'show')->name('adsense.show');
-		Route::get('/edit/{adsense}', 'edit')->name('adsense.edit');
-		Route::put('/update/{adsense}', 'update')->name('adsense.update');
-		Route::delete('/destroy/{adsense}', 'destroy')->name('adsense.destroy');
+	Route::prefix('admin/blog')->group(function () {
+		Route::resource('blog', BlogController::class);
 	});
 
-	Route::controller(DomainController::class)->prefix('admin/domain')->group(function () {
-		Route::get('/', 'index')->name('domain.index');
-		Route::get('/create', 'create')->name('domain.create');
-		Route::post('/store', 'store')->name('domain.store');
-		Route::get('/show/{domain}', 'show')->name('domain.show');
-		Route::get('/edit/{domain}', 'edit')->name('domain.edit');
-		Route::put('/update/{domain}', 'update')->name('domain.update');
-		Route::delete('/destroy/{domain}', 'destroy')->name('domain.destroy');
+	Route::prefix('admin/adsense')->group(function () {
+		Route::resource('adsense', AdsenseController::class);
 	});
 
-	Route::controller(VpsController::class)->prefix('admin/vps')->group(function () {
-		Route::get('/', 'index')->name('vps.index');
-		Route::get('/create', 'create')->name('vps.create');
-		Route::post('/store', 'store')->name('vps.store');
-		Route::get('/show/{vps}', 'show')->name('vps.show');
-		Route::get('/edit/{vps}', 'edit')->name('vps.edit');
-		Route::put('/update/{vps}', 'update')->name('vps.update');
-		Route::delete('/destroy/{vps}', 'destroy')->name('vps.destroy');
+	Route::prefix('admin/domain')->group(function () {
+		Route::resource('domain', DomainController::class);
 	});
 
-	Route::controller(ProviderController::class)->prefix('admin/provider')->group(function () {
-		Route::get('/', 'index')->name('provider.index');
-		Route::get('/create', 'create')->name('provider.create');
-		Route::post('/store', 'store')->name('provider.store');
-		Route::get('/show/{provider}', 'show')->name('provider.show');
-		Route::get('/edit/{provider}', 'edit')->name('provider.edit');
-		Route::put('/update/{provider}', 'update')->name('provider.update');
-		Route::delete('/destroy/{provider}', 'destroy')->name('provider.destroy');
+	Route::prefix('admin/vps')->group(function () {
+		Route::resource('vps', VpsController::class);
 	});
 
-	Route::controller(TopicController::class)->prefix('admin/topic')->group(function () {
-		Route::get('/', 'index')->name('topic.index');
-		Route::get('/create', 'create')->name('topic.create');
-		Route::post('/store', 'store')->name('topic.store');
-		Route::get('/show/{topic}', 'show')->name('topic.show');
-		Route::get('/edit/{topic}', 'edit')->name('topic.edit');
-		Route::put('/update/{topic}', 'update')->name('topic.update');
-		Route::delete('/destroy/{topic}', 'destroy')->name('topic.destroy');
+	Route::prefix('admin/provider')->group(function () {
+		Route::resource('provider', ProviderController::class);
+	});
+
+	Route::prefix('admin/topic')->group(function () {
+		Route::resource('topic', TopicController::class);
 	});
 });
