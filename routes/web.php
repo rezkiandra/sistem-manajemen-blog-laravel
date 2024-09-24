a9b3e21a408619d7762f30cb988493d163661614
LEVI <rezkiandra4123@gmail.com>
2024-09-24 08:38:58 +0700

    change: added domain and vps routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,20 @@
 <?php
 
 use App\Http\Controllers\AdsenseController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DomainController;
+use App\Http\Controllers\VpsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-	return view('welcome');
+Route::redirect('/', '/admin/adsense', 301);
+
+Route::middleware(['web'])->group(function () {
+	Route::controller(DashboardController::class)->prefix('admin')->group(function () {
+		Route::get('/', 'index')->name('dashboard');
+	});
 });
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['web'])->group(function () {
 	Route::controller(AdsenseController::class)->prefix('admin/adsense')->group(function () {
 		Route::get('/', 'index')->name('adsense.index');
 		Route::get('/create', 'create')->name('adsense.create');
@@ -16,5 +23,25 @@ Route::middleware(['auth'])->group(function () {
 		Route::get('/edit/{adsense}', 'edit')->name('adsense.edit');
 		Route::put('/update/{adsense}', 'update')->name('adsense.update');
 		Route::delete('/destroy/{adsense}', 'destroy')->name('adsense.destroy');
+	});
+
+	Route::controller(DomainController::class)->prefix('admin/domain')->group(function () {
+		Route::get('/', 'index')->name('domain.index');
+		Route::get('/create', 'create')->name('domain.create');
+		Route::post('/store', 'store')->name('domain.store');
+		Route::get('/show/{domain}', 'show')->name('domain.show');
+		Route::get('/edit/{domain}', 'edit')->name('domain.edit');
+		Route::put('/update/{domain}', 'update')->name('domain.update');
+		Route::delete('/destroy/{domain}', 'destroy')->name('domain.destroy');
+	});
+
+	Route::controller(VpsController::class)->prefix('admin/vps')->group(function () {
+		Route::get('/', 'index')->name('vps.index');
+		Route::get('/create', 'create')->name('vps.create');
+		Route::post('/store', 'store')->name('vps.store');
+		Route::get('/show/{vps}', 'show')->name('vps.show');
+		Route::get('/edit/{vps}', 'edit')->name('vps.edit');
+		Route::put('/update/{vps}', 'update')->name('vps.update');
+		Route::delete('/destroy/{vps}', 'destroy')->name('vps.destroy');
 	});
 });
